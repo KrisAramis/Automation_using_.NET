109d1d9342de5ea2b9de44f4b3ee1b3f583d8010
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-06-09 21:21:22 +0200

    added 2nd homework csharpbasics2

--- Program.cs
+++ Program.cs
@@ -6,11 +6,26 @@ class Program
 {
     static void Main(string[] args)
     {
-     new Homework_1().Task_1();   
+     /*new Homework_1().Task_1();   
      new Homework_1().Task_2();
      new Homework_1().Task_3();
      new Homework_1().Task_4();
-     new Homework_1().Task_5();
-     new Homework_1().Task_6();
-     }
+     new Homework_1().Task_5();*/
+     //new Homework_1().Task_6();
+     //new Homework_2().Task_1();
+     /*new Homework_2().Task_1();
+     new Homework_2().Task_2();
+     new Homework_2().Task_3();
+     new Homework_2().Task_4();
+     new Homework_2().Task_5();*/
+     new Homework_2().Task_1();
+     new Homework_2().Task_2();
+     new Homework_2().Task_3();
+     new Homework_2().Task_4();
+     new Homework_2().Task_5();
+     new Homework_2().Task_6();
+     new Homework_2().Task_7();
+     new Homework_2().Task_8();
+     new Homework_2().Task_9();
+    }
 }
