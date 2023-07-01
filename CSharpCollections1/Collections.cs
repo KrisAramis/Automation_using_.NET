92584171c7ddc0ef3b9b587fb933bbdc7e62cd70
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-07-01 20:12:41 +0200

    adding not final changes to Homework collections

--- CSharpCollections1/Collections.cs
+++ CSharpCollections1/Collections.cs
@@ -1,6 +1,49 @@
-﻿namespace Kristina_Kulich__Application.CSharpCollections1;
+﻿using System.Collections;
+using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Kristina_Kulich__Application.CSharpCollections1;
 
 public class Collections
 {
+    private int l = 0;
+    private int k = 0;
+    public static int numberofeven;
+    public static int numberofuneven;
     
+    public int[] even = new int[numberofeven];
+    public int[] uneven = new int[numberofuneven];
+    
+    public static void calculateMassiveLength(int[] massive)
+    {
+        for (int i=0; i < massive.Length; i++)
+        {
+            if (massive[i] % 2 == 0)
+            {
+                numberofeven++;
+            }
+            else
+            {
+                numberofuneven++;
+            }
+        }
+    }
+
+    public void DevideInEvenUnevenMassives(int[] massive)
+    {
+        
+        for (int i = 0; i < massive.Length-1; i++)
+        {
+            if (massive[i] % 2 == 0)
+            {
+                even[l]=massive[i];
+                l++;
+            }
+            else
+            {
+                uneven[k]=massive[i];
+                k++;
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using Kristina_Kulich__Application.CSharpCollections1;
 using Kristina_Kulich__Application.CSharpOOP2;
 using Kristina_Kulich__Application.CSharpOOP4;
 
+using System;
+using System.Collections;
+
 class Program
 {
     static void Main(string[] args)
@@ -65,7 +68,7 @@ class Program
         Car.RepaintCar(matiz);
         #endregion
 
-        #region Ierarchy classes exemplares
+        /*#region Ierarchy classes exemplares
         Cylinder cyl = new Cylinder("cylinder", 23, 10);
         cyl.ShapeInfo();
         Ball smallBall = new Ball("ball", 10);
@@ -73,7 +76,7 @@ class Program
         Pyramid egiptPyramid = new Pyramid("Faraoh", 10, 10);
         var pyramidHeight = egiptPyramid._height;
         #endregion
-        egiptPyramid.ShapeInfo();
+        egiptPyramid.ShapeInfo();*/
 
         #region Separate massive into 2
 
@@ -82,13 +85,13 @@ class Program
 
         #endregion
 
-        #region Sort Lastnames
+        /*#region Sort Lastnames
         coll.SortLastnames();
-        #endregion
+        #endregion*/
 
-        #region Citizens
+       // #region Citizens
 
-        {
+        /*{
             List<Citizen> citizensofBobruisk = new List<Citizen>();
             citizensofBobruisk.Add(new Citizen(){LastnameNameFathername = "Malinowski Henric Vladzimiravich", HomeAddress = "Malinava 24", BirthDate = new(2011,6,10)});
             citizensofBobruisk.Add(new Citizen(){LastnameNameFathername = "Macej Grink Aliaksandravich", HomeAddress = "Hrusheva 84", BirthDate = new(1991,6,10)});
@@ -105,9 +108,49 @@ class Program
             }
             Console.WriteLine(citizensofBobruisk.Where(HomeAddress => HomeAddress.Equals("Malinava 24")).ToList());
         }
+        #endregion*/
+        int[] numbers = new[] {1,2,3,4,5,6,7 };
+
+        //Collections.calculateMassiveLength(numbers);
+        //coll.DevideInEvenUnevenMassives(numbers);
+        
+        #region Task
+
+        //1. Make all cities names start with capitalized letter. Use extension and query syntax
+        //2. Create City class with property Name. Using Select make list of City objects
+
+        #endregion
+
+        #region Dictionary 
+        System.Console.WriteLine("Write 5 City-Country combinations in following format: Country:Capital City");
+        string[] InputCountries = Console.ReadLine().Split(' ');
+        string[] Countries = new string[InputCountries.Length];
+        for (int i = 0; i < InputCountries.Length; i++)
+        {
+            Countries[i]=InputCountries[i].Substring(0,InputCountries[i].IndexOf(':'));
+            Console.WriteLine(Countries[i]);
+        }
+        string[] Cities = new string[InputCountries.Length];
+        for (int i = 0; i < InputCountries.Length; i++)
+        {
+            Cities[i]=InputCountries[i].Substring(InputCountries[i].IndexOf(':')+1);
+            Console.WriteLine(Cities[i]);
+        }
+        
+        Dictionary<string, string> CountryCapitalCity = new Dictionary<string, string>();
+git 
+        for (int i = 0; i < InputCountries.Length; i++)
+        {
+            CountryCapitalCity.Add(Countries[i], Cities[i]); 
+            //When I get iside by debugging get this error below
+            //An IL variable is not available at the current native IP. The error code is CORDBG_E_IL_VAR_NOT_AVAILABLE, or 0x80131304.
+        }
+
+        foreach (KeyValuePair<string, string> kvp in CountryCapitalCity)
+        {
+            //CountryCapitalCity.Add();
+            Console.WriteLine($"Key = {0}, Value = {1}",  kvp.Key, kvp.Value);
+        }
         #endregion
     }
-
-
-    
 }
