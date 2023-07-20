cfec1c412529c4468f7db103a8f2a1cf291c9b14
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-07-20 16:05:25 +0200

    removed commented code

--- FlowerRepaint.cs
+++ FlowerRepaint.cs
@@ -5,7 +5,6 @@ namespace Kristina_Kulich__Application;
 
 public class FlowerRepaint<T> where T:Flower
 {
-   // public T Color { get; set; }
     public static void RepaintFlower( T item, string newcolor, double upsell = 0.15) 
     {
         item.Color = newcolor;
