1db27e165f20409c66a0f37a4f43ca33d84fe2bf
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-07-31 22:34:01 +0200

    added homework unitTests

---
+++ CalculatorTest/CalculatorTestNunit/TestsforCalculator.cs
@@ -0,0 +1,72 @@
+﻿using CSharpCalculator;
+using NUnit.Framework;
+
+namespace CalculatorTest;
+
+public class Tests
+{
+    [OneTimeSetUp]
+    public void ClassInitialize()
+        {
+            Console.WriteLine("Class init");
+        }
+    private Calculator _calculator;
+
+    [SetUp]
+    public void TestInitialize()
+    {
+        _calculator = new Calculator();
+    }
+    
+    [TestCase(3,4,7)]
+    [Test]
+    public void SumPositiveTest(double firstNumber, double secondNumber, double expectedSum)
+    {
+        var actualSum = _calculator.Add(firstNumber,secondNumber);
+        Assert.AreEqual(actualSum, expectedSum);
+    }
+
+    [Test]
+    public void Abs()
+    {
+        double a = -11;
+        double actualResult = _calculator.Abs(a);
+        Assert.False(actualResult == a);
+    }
+    
+    [TestCase(22, 11, 2)]
+    public void DevideTest(double firstNumber, double secondNumber, double expectedQuotient)
+    {
+        var actualQuotient = _calculator.Divide(firstNumber, secondNumber);
+        Assert.AreEqual(actualQuotient,expectedQuotient);
+    }
+    [Test]
+    [TestCase(null, 2)]
+    [TestCase(2,8)]
+    public void PowThrowsNotFiniteNumberException(double a, double b)
+    {
+        Assert.Throws<NotFiniteNumberException>(() => _calculator.Pow(a, b));
+    }
+
+    
+    [TestCase(4, 2)]
+    public void SqrtTest(double number, double expectedSqrt)
+    {
+        var actualSqrt = _calculator.Sqrt(number);
+        Assert.AreEqual(actualSqrt,expectedSqrt);
+    }
+
+    [TestCase(90, 0.89399666360055785)]
+    public void SinTest(double angle, double expectedResult)
+    {
+        double actualResult = Math.Sin(angle);
+        Assert.AreEqual(actualResult,expectedResult);
+    }
+
+    [Test]
+    public void NegativeValueExceptionTest()
+    {
+        string number = "some";
+        Assert.Throws<NotFiniteNumberException>(() => _calculator.isNegative(number));
+    }
+}
---
+++ CalculatorTest/CalculatorTestsXUnit/TestsfoCalculatorPart2.cs
@@ -0,0 +1,109 @@
+using System.Runtime.CompilerServices;
+using CSharpCalculator;
+using Xunit;
+using Xunit.Sdk;
+
+namespace TestProject1;
+
+public class CalculatorTests
+{
+    private Calculator _calculatorInstance;
+
+    public CalculatorTests()
+    {
+        _calculatorInstance = new Calculator();
+    }
+
+    [Fact]
+    public void NotNegativeFalseWhenPositive()
+    {
+        object a = 4;
+        bool result = _calculatorInstance.isNegative(a);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void PositiveCatchException()
+    {
+        string a = "summer";
+        Assert.Throws<NotFiniteNumberException>(() => _calculatorInstance.isPositive(a));
+    }
+
+    [Fact]
+    public void AbsReturnsCorrect()
+    {
+        double expected = 3;
+        object a = -3;
+        double result = _calculatorInstance.Abs(a);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void SubReturnsCorrect()
+    {
+        double a = 0;
+        double b = 3;
+        double actualResult = _calculatorInstance.Sub(a, b);
+        double expectedResult = -3;
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void CosReturnsCorrect()
+    {
+        double angle = -30;
+        double expectedResult = Math.Cos(angle);
+        double actualResult = _calculatorInstance.Cos(angle);
+        Assert.True(actualResult == expectedResult);
+    }
+
+    [Theory]
+    [InlineData(2, 3)]
+    public void PowReturnsCorrect(int firstNumber, double secondNumber)
+    {
+        double expectedResult = 8;
+        double actualResult = _calculatorInstance.Pow(firstNumber, secondNumber);
+        Assert.Equal(expectedResult, actualResult);
+    }
+    
+    [Theory]
+    [InlineData(2, 1)]
+    public void AddReturnsCorrect(double firstNumber, double secondNumber)
+    {
+        double expectedResult = 3;
+        double actualResult = _calculatorInstance.Add(firstNumber, secondNumber);
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void CheckCosException()
+    {
+        string a = "tree";
+        Assert.Throws<NotFiniteNumberException>(()=>_calculatorInstance.Cos(a));
+    }
+
+    [Theory]
+    [InlineData(2, 3, 4)]
+    public void CheckNegativeMultiply(double firstNumber, double secondNumber, double expectedResult)
+    {
+        double actualResult = _calculatorInstance.Multiply(firstNumber, secondNumber);
+        Assert.NotEqual(actualResult,expectedResult);
+    }
+
+    [Fact]
+    public void CheckSqrt()
+    {
+        double expectedResult = 4;
+        double number = 16;
+        double actualResult = _calculatorInstance.Sqrt(number);
+        Assert.Equal(expectedResult,actualResult);
+    }
+
+    [Theory]
+    [InlineData(2,0)]
+    public void DivideReturnsNotInt(double firstNumber,double secondNumber )
+    {
+        var expectedResult = _calculatorInstance.Divide(firstNumber, secondNumber);
+        Assert.IsNotType<Int16>(expectedResult);
+    }
+}
