7f1a0177780721959f33a53f5a1fb64ea61a3042
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-08-20 20:01:55 +0200

    adding changes after review

--- CalculatorTest/Selenium_Basics/EpamNavigationTests.cs
+++ CalculatorTest/Selenium_Basics/EpamNavigationTests.cs
@@ -1,12 +1,13 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Selenium_Basics;
 
-public class EpamWebsitesTests
+public class EpamNavigationTests
 {
     private IWebDriver _driver;
-    private const string _url = "https://www.epam.com";
+    private const string _epamLandingUrl = "https://www.epam.com";
     
     [SetUp]
     public void SetupBrowser()
@@ -18,20 +19,16 @@ public class EpamWebsitesTests
     [Test]
     public void VerifyStartPageUrl()
     {
-        _driver.Navigate().GoToUrl(_url);
-        string currentUrl;
-        string expectedUrl = "https://www.epam.com";
-        currentUrl = _driver.Url;
-        Assert.That(currentUrl, Is.EqualTo(expectedUrl));
+        _driver.Navigate().GoToUrl(_epamLandingUrl);
+        var currentUrl = _driver.Url;
+        Assert.That(currentUrl, Is.EqualTo(_epamLandingUrl),"Page URL isn't correct");
     }
 
     [Test]
-    public void VerifyHowWeDoItOurWork()
+    public void VerifyNavigationBackTest()
     {
         string urlHowWeDoIt = "https://www.epam.com/how-we-do-it";
         string ourWork = "https://www.epam.com/our-work";
-        _driver.Url = "https://www.epam.com" ;
-        _driver.Navigate().GoToUrl(_url);
         _driver.Navigate().GoToUrl(urlHowWeDoIt);
         _driver.Navigate().GoToUrl(ourWork);
         _driver.Navigate().Refresh();
